f164d083da8928c520abfd5ab26190888eff3073
shin-mallang <huipulci1@naver.com>
2023-11-13 23:22:23 +0900

    [#1] test: 인수테스트를 위한 공통 로직 작성

---
+++ src/test/java/com/oneandahalf/backend/acceptance/AcceptanceTest.java
@@ -0,0 +1,26 @@
+package com.oneandahalf.backend.acceptance;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import com.oneandahalf.backend.common.DataClearExtension;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@DisplayNameGeneration(ReplaceUnderscores.class)
+@ExtendWith(DataClearExtension.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public abstract class AcceptanceTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    protected void setUp() {
+        RestAssured.port = port;
+    }
+}
