bcd996dc5bba4104c62c1613551992b155981e45
shin-mallang <huipulci1@naver.com>
2023-11-14 12:42:34 +0900

    [#13] feat: 거래 제안 서비스 구현

---
+++ src/main/java/com/oneandahalf/backend/trade/application/TradeSuggestionService.java
@@ -0,0 +1,33 @@
+package com.oneandahalf.backend.trade.application;
+
+import com.oneandahalf.backend.member.domain.Member;
+import com.oneandahalf.backend.member.domain.MemberRepository;
+import com.oneandahalf.backend.product.domain.Product;
+import com.oneandahalf.backend.product.domain.ProductRepository;
+import com.oneandahalf.backend.trade.application.command.SuggestTradeCommand;
+import com.oneandahalf.backend.trade.domain.TradeSuggestion;
+import com.oneandahalf.backend.trade.domain.TradeSuggestionRepository;
+import com.oneandahalf.backend.trade.domain.TradeSuggestionValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class TradeSuggestionService {
+
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
+    private final TradeSuggestionRepository tradeSuggestionRepository;
+    private final TradeSuggestionValidator tradeSuggestionValidator;
+
+    public Long suggest(SuggestTradeCommand command) {
+        Member suggester = memberRepository.getById(command.suggesterId());
+        Product product = productRepository.getById(command.productId());
+        TradeSuggestion tradeSuggestion = new TradeSuggestion(suggester, product);
+        tradeSuggestion.suggest(tradeSuggestionValidator);
+        return tradeSuggestionRepository.save(tradeSuggestion)
+                .getId();
+    }
+}
