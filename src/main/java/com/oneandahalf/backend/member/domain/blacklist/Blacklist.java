d24d1c2baafb6a8d7c1969f5ba64a71cad746df2
shin-mallang <huipulci1@naver.com>
2023-11-16 01:38:27 +0900

    [#9] feat: 블랙리스트 도메인 구현

---
+++ src/main/java/com/oneandahalf/backend/member/domain/blacklist/Blacklist.java
@@ -0,0 +1,25 @@
+package com.oneandahalf.backend.member.domain.blacklist;
+
+import com.oneandahalf.backend.common.domain.CommonDomainModel;
+import com.oneandahalf.backend.member.domain.Member;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Blacklist extends CommonDomainModel {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+    
+    public Blacklist(Member member) {
+        this.member = member;
+    }
+}
---
+++ src/main/java/com/oneandahalf/backend/member/domain/blacklist/BlacklistRepository.java
@@ -0,0 +1,6 @@
+package com.oneandahalf.backend.member.domain.blacklist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BlacklistRepository extends JpaRepository<Blacklist, Long> {
+}
