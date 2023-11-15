d8a45a08047026e7b1b68e4079bbba0682d5a449
shin-mallang <huipulci1@naver.com>
2023-11-16 02:07:59 +0900

    [#9] feat: 블랙리스트 접근방지 기능 추가

--- src/main/java/com/oneandahalf/backend/member/domain/blacklist/BlacklistRepository.java
+++ src/main/java/com/oneandahalf/backend/member/domain/blacklist/BlacklistRepository.java
@@ -1,6 +1,15 @@
 package com.oneandahalf.backend.member.domain.blacklist;
 
+import com.oneandahalf.backend.common.exception.NotFoundEntityException;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlacklistRepository extends JpaRepository<Blacklist, Long> {
+
+    default Blacklist getByMemberId(Long memberId) {
+        return findByMemberId(memberId).orElseThrow(() ->
+                new NotFoundEntityException("회원 id가 %d인 회원은 블랙리스트가 아닙니다."));
+    }
+
+    Optional<Blacklist> findByMemberId(Long memberId);
 }
--- src/main/java/com/oneandahalf/backend/member/presentation/support/OptionalAuthInterceptor.java
+++ src/main/java/com/oneandahalf/backend/member/presentation/support/OptionalAuthInterceptor.java
@@ -2,6 +2,8 @@ package com.oneandahalf.backend.member.presentation.support;
 
 import static com.oneandahalf.backend.member.presentation.support.AuthConstant.SESSION_ATTRIBUTE_MEMBER_ID;
 
+import com.oneandahalf.backend.member.domain.blacklist.BlacklistRepository;
+import com.oneandahalf.backend.member.exception.ForbiddenBlacklistException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Optional;
@@ -13,6 +15,7 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class OptionalAuthInterceptor implements HandlerInterceptor {
 
+    private final BlacklistRepository blacklistRepository;
     private final AuthContext authContext;
 
     @Override
@@ -20,7 +23,21 @@ public class OptionalAuthInterceptor implements HandlerInterceptor {
         Optional.ofNullable(request.getSession(false))
                 .map(it -> it.getAttribute(SESSION_ATTRIBUTE_MEMBER_ID))
                 .map(id -> (Long) id)
-                .ifPresent(authContext::setMemberId);
+                .ifPresent(id -> {
+                    checkBlackList(request, id);
+                    authContext.setMemberId(id);
+                });
         return true;
+    }
+
+    private void checkBlackList(HttpServletRequest request, Long id) {
+        if (blacklistRepository.findByMemberId(id).isPresent()) {
+            Optional.ofNullable(request.getSession(false))
+                    .ifPresent(session -> {
+                        session.removeAttribute(SESSION_ATTRIBUTE_MEMBER_ID);
+                        session.invalidate();
+                    });
+            throw new ForbiddenBlacklistException();
+        }
     }
 }
