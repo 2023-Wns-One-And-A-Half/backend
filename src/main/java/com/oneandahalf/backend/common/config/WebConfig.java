5120f4569f8ecbc9ec58c820e6dc3685549a3416
shin-mallang <huipulci1@naver.com>
2023-11-17 20:45:57 +0900

    [#47] feat: CORS 수정 1

--- src/main/java/com/oneandahalf/backend/common/config/WebConfig.java
+++ src/main/java/com/oneandahalf/backend/common/config/WebConfig.java
@@ -1,10 +1,14 @@
 package com.oneandahalf.backend.common.config;
 
-import static org.springframework.http.HttpMethod.*;
+import static org.springframework.http.HttpMethod.DELETE;
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
+import static org.springframework.http.HttpMethod.PATCH;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.PUT;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,6 +20,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*") // HTTPS 를 사용하지 않으므로 편의를 위해 *로 설정
+                .allowedHeaders("*")
                 .allowedMethods(
                         OPTIONS.name(),
                         GET.name(),
